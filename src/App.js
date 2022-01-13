0dd625c044450d081c8a35edab6dd775834ab06f
elicuene <emc78@duke.edu>
2022-01-13 11:23:13 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react';
+import axios from 'axios'
+import './styles.css';
 
-function App() {
+
+export default function App() {
+  
+  const clientID = "058ffdb2354e43729cc7cb8e921e49c3"
+  const redirectURI = "http://localhost:3000/"
+  const authEndpoint = "https://accounts.spotify.com/authorize"
+  const responseType = "token"
+
+  const [token, setToken] = useState("")
+  const [searchKey, setSearchKey] = useState("")
+  const [tracks, setTracks] = useState([])
+  const [selectedTrackID, setSelectedTrackID] = useState("")
+  
+  useEffect(() => {
+    const hash = window.location.hash
+    let tok = window.localStorage.getItem("token")
+
+    if (!tok && hash) {
+      tok = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+      window.location.hash = ""
+      window.localStorage.setItem("token", tok)
+    }
+
+    setToken(tok)
+ 
+  }, [])
+
+  const searchTracks = async (e) => {
+    e.preventDefault()
+    const {data} = await axios.get("https://api.spotify.com/v1/search/", {
+        headers: {
+          Authorization: `Bearer ${token}`
+        },
+        params: {
+          q: searchKey,
+          type: "track"
+        }
+    })
+    setTracks(data.tracks.items)    
+  }
+
+  const tracksElements = tracks.map(track => {
+    return (
+      <div className="displayed-track" key={track.id}>{track.name} by {track.artists[0].name}</div>
+    )
+
+  })
+
+  // function selectTrack(e) {
+  //   setSelectedTrackID(e.target.value)
+  // }
+
+  function logout() {
+    setToken("")
+    window.localStorage.removeItem("token")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!token ? <a href={`${authEndpoint}?client_id=${clientID}&redirect_uri=${redirectURI}&response_type=${responseType}`}>Login to Spotify</a> : "Logged in!"}
+      {token && <form onSubmit={searchTracks}>
+        <label>Search Songs:
+          <input type="text"
+                value={searchKey}
+                onChange={(e) => setSearchKey(e.target.value)}
+          />
+        </label>
+        <input type="submit" />
+      </form>}
+      {token && <button onClick={logout}>Log Out</button>}
+      {token && tracksElements}
     </div>
   );
 }
 
-export default App;
